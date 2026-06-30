7ed8257cc9057d887055a2ff735be3718e3c6891
unknown <mr09991999@outlook.com>
2026-06-30 21:28:10 +0800

    new version complete

---
+++ AudioDatas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FastModdingLib.Audio;
+
+namespace DuckovWeaponExample
+{
+    public static class AudioDatas
+    {
+        public static AudioData mk23_shoot = new AudioData
+        {
+            Path = "assets/sounds/shoot_mk23.ogg",
+            Eventname = "SFX/Combat/Gun/Shoot/shoot_mk23",
+            MinDistance = 1.0F,
+            MaxDistance = 50.0F,
+        };
+        public static AudioData mk23_shoot_mute = new AudioData
+        {
+            Path = "assets/sounds/shoot_mk23_mute.ogg",
+            Eventname = "SFX/Combat/Gun/Shoot/shoot_mk23_mute",
+            MinDistance = 1.0F,
+            MaxDistance = 50.0F,
+        };
+
+        public static AudioData mk23_reload = new AudioData
+        {
+            Path = "assets/sounds/mag_mk23_start.ogg",
+            Eventname = "SFX/Combat/Gun/Reload/mag_mk23_start",
+            MinDistance = 1.0F,
+            MaxDistance = 50.0F,
+        };
+        public static AudioData mk23_reload_end = new AudioData
+        {
+            Path = "assets/sounds/mag_mk23_end.ogg",
+            Eventname = "SFX/Combat/Gun/Reload/mag_mk23_end",
+            MinDistance = 1.0F,
+            MaxDistance = 50.0F,
+        };
+    }
+}
--- ModBehaviour.cs
+++ ModBehaviour.cs
@@ -1,45 +1,38 @@
-﻿using Duckov.UI;
-using Duckov.Utilities;
+﻿
 using FastModdingLib;
+using FastModdingLib.Audio;
+using FastModdingLib.Utils;
 using ItemStatsSystem;
 using System;
 using System.Reflection;
-using TMPro;
 using Unity.VisualScripting;
-using Unity.VisualScripting.FullSerializer;
-using UnityEngine;
-using UnityEngine.UI;
 
 namespace DuckovWeaponExample
 {
-    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    public class ModBehaviour : Duckov.Modding.ModBehaviour, IHasModid
     {
-
-        string dllPath = Assembly.GetExecutingAssembly().Location;
-        string modid = "DuckovWeaponExample";
-        void Awake()
-        {
-            Debug.Log($"DuckovWeaponExample awaked. Presented by Zaia");
-
-            I18n.InitI18n(dllPath);
-        }
-        protected override void OnBeforeDeactivate()
-        {
-
-        }
-
+        readonly string dllPath = Assembly.GetExecutingAssembly().Location;
+        readonly string modid = "DuckovWeaponExample";
+        public string GetModid() => modid;
         protected override void OnAfterSetup()
         {
-            //AssetUtil.Start();
+            ModPathResolver.Register(modid, dllPath);
+            
+            I18n.InitI18n(modid);
+            RegisterAudios();
             RegisterItems();
             RegisterQuests();
-
-            I18n.loadFileJson(dllPath, $"/{I18n.localizedNames[SodaCraft.Localizations.LocalizationManager.CurrentLanguage]}");
             AddFormulas();
             AddShopItems();
         }
-
-        private static void AddShopItems()
+        private void RegisterAudios()
+        {
+            AudioUtil.Instance.RegisterAudio(new Identifier(modid,"mk23_shoot"), AudioDatas.mk23_shoot);
+            AudioUtil.Instance.RegisterAudio(new Identifier(modid, "mk23_shoot_mute"), AudioDatas.mk23_shoot_mute);
+            AudioUtil.Instance.RegisterAudio(new Identifier(modid, "mk23_reload"), AudioDatas.mk23_reload);
+            AudioUtil.Instance.RegisterAudio(new Identifier(modid, "mk23_reload_end"), AudioDatas.mk23_reload_end);
+        }
+        private void AddShopItems()
         {
             ShopGoodsData data = new ShopGoodsData
             {
@@ -51,7 +44,7 @@ namespace DuckovWeaponExample
                 possibility = 1F
             };
 
-            ShopUtils.AddGoods(data);
+            ShopUtils.AddGoods(data, modid);
 
             ShopGoodsData data1 = new ShopGoodsData
             {
@@ -63,7 +56,7 @@ namespace DuckovWeaponExample
                 possibility = 1F
             };
 
-            ShopUtils.AddGoods(data1);
+            ShopUtils.AddGoods(data1, modid);
 
             ShopGoodsData data2 = new ShopGoodsData
             {
@@ -75,7 +68,7 @@ namespace DuckovWeaponExample
                 possibility = 1F
             };
 
-            ShopUtils.AddGoods(data2);
+            ShopUtils.AddGoods(data2, modid);
 
             ShopGoodsData data3 = new ShopGoodsData
             {
@@ -87,7 +80,7 @@ namespace DuckovWeaponExample
                 possibility = 1F
             };
 
-            ShopUtils.AddGoods(data3);
+            ShopUtils.AddGoods(data3, modid);
 
             ShopGoodsData data4 = new ShopGoodsData
             {
@@ -99,58 +92,89 @@ namespace DuckovWeaponExample
                 possibility = 1F
             };
 
-            ShopUtils.AddGoods(data4);
+            ShopUtils.AddGoods(data4, modid);
+
+            ShopGoodsData data5 = new ShopGoodsData
+            {
+                merchantProfileID = "Merchant_Weapon",
+                typeID = 31003,
+                maxStock = 1,
+                forceUnlock = true,
+                priceFactor = 1F,
+                possibility = 1F
+            };
+
+            ShopUtils.AddGoods(data5, modid);
         }
 
-        private static void RegisterQuests()
+        private void RegisterQuests()
         {
-            QuestUtils.RegisterQuest(Quests.EXAMPLE_QUEST_1);
-            QuestUtils.RegisterQuest(Quests.EXAMPLE_QUEST_2);
+            QuestUtils.RegisterQuest(Quests.EXAMPLE_QUEST_1, modid);
+            QuestUtils.RegisterQuest(Quests.EXAMPLE_QUEST_2, modid);
             QuestUtils.AddQuestRelation(31001, before: 23, after: 31002);
             QuestUtils.AddQuestRelation(31002, before: 31001);
         }
 
         private void RegisterItems()
         {
-            var Bundle = AssetUtil.LoadBundle(dllPath, "examplebundle");
+            var Bundle = AssetUtil.LoadBundle(new Identifier(modid, "examplebundle"));
             if (Bundle != null)
             {
-                ItemUtils.RegisterGun(Bundle, "LMRItem", 654, modid);
+                ItemUtils.RegisterGun(new Identifier(modid, "lmr_item"), Bundle, "LMRItem", 654);
+                ItemUtils.RegisterGun(new Identifier(modid, "mk23_item"), Bundle, "MK23Item", 783);
 
-                Item stock = ItemUtils.GetCustomItem(dllPath, Items.STOCK_TEST, modid);
+                Item stock = ItemUtils.GetCustomItem(new Identifier(modid, "stock_test"), Items.STOCK_TEST);
                 ItemUtils.SetItemGraphic(stock, Bundle, "IG_STOCK_TEST");
-                ItemUtils.RegisterItem(stock, modid);
+                ItemUtils.RegisterItem(new Identifier(modid, "stock_test"), stock);
 
-                Item sight = ItemUtils.GetCustomItem(dllPath, Items.SIGHT_TEST, modid);
+                Item sight = ItemUtils.GetCustomItem(new Identifier(modid, "sight_test"), Items.SIGHT_TEST);
                 ItemUtils.SetItemGraphic(sight, Bundle, "IG_SIGHT_TEST");
 
                 ItemSetting_Accessory itemSetting_Accessory = sight.AddComponent<ItemSetting_Accessory>();
                 itemSetting_Accessory.overrideAdsAimMarker = ItemAssetsCollection.GetPrefab(571).GetComponent<ItemSetting_Accessory>().overrideAdsAimMarker;
-                ItemUtils.RegisterItem(sight, modid);
+                ItemUtils.RegisterItem(new Identifier(modid, "sight_test"), sight);
 
-                Item muzzle = ItemUtils.GetCustomItem(dllPath, Items.MUZZLE_TEST, modid);
+                Item muzzle = ItemUtils.GetCustomItem(new Identifier(modid, "muzzle_test"), Items.MUZZLE_TEST);
                 ItemUtils.SetItemGraphic(muzzle, Bundle, "IG_MUZZLE_TEST");
-                ItemUtils.RegisterItem(muzzle, modid);
+                ItemUtils.RegisterItem(new Identifier(modid, "muzzle_test"), muzzle);
 
-                Item grip = ItemUtils.GetCustomItem(dllPath, Items.GRIP_TEST, modid);
+                Item grip = ItemUtils.GetCustomItem(new Identifier(modid, "grip_test"), Items.GRIP_TEST);
                 ItemUtils.SetItemGraphic(grip, Bundle, "IG_GRIP_TEST");
-                ItemUtils.RegisterItem(grip, modid);
+                ItemUtils.RegisterItem(new Identifier(modid, "grip_test"), grip);
             }
-            ItemUtils.CreateCustomBluePrint(Quests.BLUEPRINT_EXAMPLE);
+
+            ItemUtils.CreateCustomBluePrint(new Identifier(modid, "lmr_blueprint"), Quests.BLUEPRINT_EXAMPLE);
         }
 
         private void AddFormulas()
         {
-            CraftingUtils.AddCraftingFormula("lmr_crafting_example", 0L, new (int, long)[]
-            {
-                (367, 5L)
-            }, 31001, 1, new string[1] { "WorkBenchAdvanced" }, "", false, false, false, modid
-
+            CraftingUtils.AddCraftingFormula(new CraftingFormulaData {
+                Id = new Identifier(modid, "lmr_crafting_example"), 
+                Money = 0L,
+                CostItems = new[]
+                {
+                ItemEntry.Of(367, 5)
+                },
+                Result = ItemEntry.Of(new Identifier(modid, "lmr_item"), 1),
+                Tags = new string[] { "WorkBenchAdvanced" },
+                RequirePerk = "",
+                UnlockByDefault = false,
+                HideInIndex = false,
+                LockInDemo = false
+            }
+                
             );
-            CraftingUtils.AddDecomposeFormula(31001, 0L, new (int, long)[]
+
+            CraftingUtils.AddDecomposeFormula(new DecomposeFormulaData 
             {
-                (367, 1L)
-            });
+                Id = new Identifier(modid, "scrap_old_gun"),
+                SourceItemId = new Identifier(modid, "lmr_item"),  // 被分解物品
+                Money = 50,
+                ResultItems = new[] {
+                    ItemEntry.Of(367, 1)
+                }
+            }
+            );
         }
 
         void OnDestroy()
